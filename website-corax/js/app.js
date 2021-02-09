9f03b94220670b8fc14a0177375c3f471558931e
devidwolf <68775926+devidwolf@users.noreply.github.com>
2021-02-09 10:47:12 +0100

    add images, finalize overall styling

--- website-corax/js/app.js
+++ website-corax/js/app.js
@@ -12,10 +12,9 @@ let about = new Vue({
     headline: 'Wir haben uns mit dem besten Designer der Welt zusammengetan: Der Natur.',
     blocks: [{
         subtitle: 'Wir lieben Holz.',
-        text: 'Am Anfang des sinnvollen Einsatzes von Ressourcen steht der nachhaltig bewirtschaftete Wald, der mit der Kraft der Sonne immer wieder nachwächst. Das edle, europäische Laubholz verarbeiten wir mit größter Sorgfalt und Achtsamkeit zu einzigartigen Designmöbeln, die unsere Liebe zum Holz in jedem einzelnen Detail spürbar machen. Nachhaltigkeit und Verantwortung, Wertschöpfung im Einklang mit Mensch und Natur, bestimmen bei CORAX das Denken und Handeln.'
-      },
-      {
-        text: 'Die Kombination der beiden ursprünglichsten Materialien die die Natur hervorgebracht hat, machen unsere Möbelstücke einzigartig und langlebig. Versprochen.'
+        image: './img/holz.webp',
+        text: 'Am Anfang des sinnvollen Einsatzes von Ressourcen steht der nachhaltig bewirtschaftete Wald, der mit der Kraft der Sonne immer wieder nachwächst. Das edle, europäische Laubholz verarbeiten wir mit größter Sorgfalt und Achtsamkeit zu einzigartigen Designmöbeln, die unsere Liebe zum Holz in jedem einzelnen Detail spürbar machen. Nachhaltigkeit und Verantwortung, Wertschöpfung im Einklang mit Mensch und Natur, bestimmen bei CORAX das Denken und Handeln.\
+        Die Kombination der beiden ursprünglichsten Materialien die die Natur hervorgebracht hat, machen unsere Möbelstücke einzigartig und langlebig. Versprochen.'
       },
       {
         text: 'Unsere Materialien stammen aus Mitteleuropa und werden regional verarbeitet. \
@@ -23,19 +22,23 @@ let about = new Vue({
       },
       {
         subtitle: 'So wird Einrichten zum Kinderspiel.',
+        image: './img/einrichten.webp',
         text: 'Ob attraktives Einzelmöbel, offene Komponente in den Wohnprogrammen oder optional mit Glastüren in Küchen und Beimöbel integriert - das Gestaltungselement spannt den roten Faden durch alle Wohnbereiche und bietet Ihnen jede Menge kreativen Freiraum bei der individuellen Gestaltung Ihres Zuhauses. Und dabei sind die flexiblen Naturholzkorpusse der Gestaltungselemente buchstäblich offen für alles.'
       },
       {
         subtitle: 'Alles aus einer Hand. Nämlich unserer.\
         Arbeitgeber mit Holz, Metall und Seele.',
+        image: './img/hand.webp',
         text: 'Wir sind ein kleiner Arbeitgeber in der Region. Die Aus- und Weiterbildung unserer Mitarbeiter sind uns sehr wichtig. Wir sehen die Menschen und ihr Können immer als Ganzes und versuchen Kompetenzen zu steigern, Perspektiven und Aufstiegschancen zu geben. So fördern wir auch ältere Mitarbeiter und helfen, Arbeit und Familie vereinbar zu machen.'
       },
       {
         subtitle: 'Einkauf mit der richtigen Einstellung.',
+        image: './img/einkauf.webp',
         text: 'Wir legen Wert auf regionale Einkaufsstrukturen. Dabei achten wir als Möbelhersteller penibel darauf, dass auch unsere Lieferanten die Werte unseres Unternehmens mittragen. Außerdem ist neben den kurzen Wegen und der entsprechend geringeren Schadstoff- und CO2-Belastung auch die Flexibilität eines regionalen Lieferantennetzwerkes ein wichtiger Bestandteil der betrieblichen Leistungsfähigkeit von CORAX. Ein weiterer positiver Effekt liegt in der Schaffung und Erhaltung von hochwertigen Arbeitsplätzen in der Region.'
       },
       {
         subtitle: 'Von A wie Ankleide bis Z wie Zen-Zimmer.',
+        image: './img/a-z1.webp',
         text: 'Wir fertigen Massivholzmöbel für alle Wohnbereiche. Egal ob Küche, Essen, Wohnen, Schlafen oder Kind: Wir haben das Richtige für alle Räume in Ihrem Zuhause. Und dabei messen wir uns in jedem Bereich mit den Besten. Unser umfassendes Know-how und die Synergieeffekte durch das breite Angebot helfen uns dabei, Ihr Zuhause immer als Ganzes und z.B. auch im Kontext der Architektur zu sehen.'
       }
     ]
@@ -47,23 +50,24 @@ let coreValues = new Vue({
   data: {
     headline: 'Die Kernwerte',
     blocks: [{
-      subtitle: 'Nachwachsende Rohstoffe',
+        subtitle: 'Nachwachsende Rohstoffe',
+        image: './img/nachwachsend.webp',
         text: 'Wir finden, dass uns die Natur mit vielen Rohstoffen beschenkt hat, aus denen man wunderbar Möbel schaffen kann. Wir versuchen deshalb, die Natur so gut wie möglich an dich weiterzugeben: Natürliche Materialien sind unser Grundwerkstoff - ganz ohne Schadstoffe.'
       },
       {
         subtitle: 'Von A bis Z durchdacht',
+        image: './img/a-z2.webp',
         text: 'Vom Design über den Herstellungsprozess bis hin zur Lagerhaltung durchleuchten wir jeden einzelnen Schritt und versuchen dabei, ein bestmögliches Verhältnis zwischen Qualität und Preis zu schaffen.'
       },
       {
         subtitle: 'Sinneserlebnis Coraxdentity',
+        image: './img/sinneserlebnis.webp',
         text: 'Der turbulente Alltag bringt schon genug Anstrengungen mit sich. Da sind unsere Möbel der perfekte Ausgleich: Natur die man spürt, Natur die man riecht. Das Gesamtkonzept ergibt ein pures Sinneserlebnis.\
-        Kurz gesagt:  CORAXDENTITY'
+        Kurz gesagt:  Coraxdentity.'
       },
       {
-        text: 'Damit dein Möbelstück so lange wie möglich so bleibt wie du es bekommen hast, haben wir eine Corax Care Box zusammengestellt.'
-      },
-      {
-        text: 'Diese beinhaltet die Corax Care Instructions - eine ausführlich Pflegeanleitung. Mit einem Reinigungspad für Verunreinigungen am Holz, einem Naturölfläschchen und einem Stofftuch kannst du von Zeit zu Zeit deine Möbel nachölen und kleine Ausbesserungen vornehmen.'
+        text: 'Damit dein Möbelstück so lange wie möglich so bleibt wie du es bekommen hast, haben wir eine Corax Care Box zusammengestellt.\
+        Diese beinhaltet die Corax Care Instructions - eine ausführlich Pflegeanleitung. Mit einem Reinigungspad für Verunreinigungen am Holz, einem Naturölfläschchen und einem Stofftuch kannst du von Zeit zu Zeit deine Möbel nachölen und kleine Ausbesserungen vornehmen.'
       },
     ]
   }
@@ -81,7 +85,7 @@ let legal = new Vue({
       },
       {
         'label': 'Öffnungszeiten',
-        'content': 'MO - DO (8:00 - 17:00), FR (8:00 - 12:00)',
+        'content': 'Mo - Do (8:00 - 17:00), Fr (8:00 - 12:00)',
       },
       {
         'label': 'Telefon',
